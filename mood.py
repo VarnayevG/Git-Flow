da770bd9fd9458de151f6b54f3b79ea83386ba7d
Глеб <varnayev.pavlodar@gmail.com>
2022-03-02 22:12:16 +0300

    Add good mood option list

--- mood.py
+++ mood.py
@@ -1,10 +1,8 @@
 def ask_about_mood(name):
     print(f"How's it going, {name}?")
     ans = input()
-    if "good" in ans.lower() \
-        or "fine" in ans.lower() \
-        or "ok" in ans.lower() \
-        or "alright" in ans.lower():
+    good_moods = ['ok', 'fine', 'good', 'alright']
+    if ans.lower() in good_moods:
         return 1
     else:
         return 0
