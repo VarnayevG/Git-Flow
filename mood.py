f17c709d94e4e00d97ee31adcbe25df91c4cfd84
Глеб <varnayev.pavlodar@gmail.com>
2022-03-02 22:04:23 +0300

    Add mood options

--- mood.py
+++ mood.py
@@ -1,5 +1,12 @@
 def ask_about_mood(name):
     print(f"How's it going, {name}?")
     ans = input()
-    return ans
+    if "good" in ans.lower() \
+        or "fine" in ans.lower() \
+        or "ok" in ans.lower() \
+        or "alright" in ans.lower():
+        return 1
+    else:
+        return 0
 
+print(ask_about_mood('Gleb'))
