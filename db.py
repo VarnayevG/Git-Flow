bd5f5a7178a61708bdcae57d9d1c7c00e24154b8
Глеб <varnayev.pavlodar@gmail.com>
2022-03-03 17:19:02 +0300

    Add write_to_db function

--- db.py
+++ db.py
@@ -1,6 +1,14 @@
+from operator import index
 import pandas as pd
 
 
 def create_db(db_name):
     df = pd.DataFrame(columns=["name", "mood"])
+    df.to_csv(db_name, index=False)
+
+
+def write_to_db(db_name, name, mood):
+    df = pd.read_csv(db_name)
+    df['name'] = name
+    df['mood'] = mood
     df.to_csv(db_name, index=False)
