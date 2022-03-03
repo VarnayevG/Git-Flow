4295b04225a91cd90003bca194a8c264fffe5449
Глеб <varnayev.pavlodar@gmail.com>
2022-03-03 17:42:10 +0300

    Add specific suggestion based on a mood

--- suggest.py
+++ suggest.py
@@ -1,11 +1,13 @@
+import random
+
 def suggest(mood_flag):
-    good_mood_suggestion = [
+    good_mood_suggestions = [
         "work on your projects",
         "go out with your friends",
         "go see a movie",
         "do sports",
     ]
-    
+
     bad_mood_suggestions = [
         "go get some sleep",
         "get some fresh air",
@@ -14,8 +16,10 @@ def suggest(mood_flag):
     ]
 
     if mood_flag == 1:
-        print("That's good! Why don't you...?")
+        suggestion = random.choice(good_mood_suggestions)
+        print(f"That's good! Why don't you {suggestion}?")
     elif mood_flag == 0:
-        print("Sorry to hear that. Maybe you should...")
+        suggestion = random.choice(bad_mood_suggestions)
+        print(f"Sorry to hear that. Maybe you should {suggestion}")
     else:
         print("Incorrect flag")
