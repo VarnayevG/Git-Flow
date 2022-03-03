b7113060c386a428ef62c74624de8610a27b989b
Глеб <varnayev.pavlodar@gmail.com>
2022-03-03 17:39:36 +0300

    Add various suggestions based on a mood

--- suggest.py
+++ suggest.py
@@ -1,7 +1,18 @@
-from xml.dom.expatbuilder import ElementInfo
-
-
 def suggest(mood_flag):
+    good_mood_suggestion = [
+        "work on your projects",
+        "go out with your friends",
+        "go see a movie",
+        "do sports",
+    ]
+    
+    bad_mood_suggestions = [
+        "go get some sleep",
+        "get some fresh air",
+        "drink some tea",
+        "talk to your friends",
+    ]
+
     if mood_flag == 1:
         print("That's good! Why don't you...?")
     elif mood_flag == 0:
